269fc9ff323cc4df77bf80e0c30ae3103664df31
Kevin Chan <ka-hin-kevin.chan@polymtl.ca>
2019-02-24 13:18:45 -0500

    ng g c avance-voyage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule, Routes } from '@angular/router';
 import { ArchiveComponent } from './archive/archive.component';
+import { AvanceVoyageComponent } from './avance-voyage/avance-voyage.component';
 
 const appRoutes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
@@ -19,7 +20,8 @@ const appRoutes: Routes = [
     AppComponent,
     HeaderComponent,
     DashboardComponent,
-    ArchiveComponent
+    ArchiveComponent,
+    AvanceVoyageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/formulaires/avance-voyage/avance-voyage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-avance-voyage',
+  templateUrl: './avance-voyage.component.html',
+  styleUrls: ['./avance-voyage.component.css']
+})
+export class AvanceVoyageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
