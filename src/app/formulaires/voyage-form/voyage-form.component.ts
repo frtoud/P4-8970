e7b5b4693140c94bf2f7f6a0707432129a8a5e53
Kevin Chan <ka-hin-kevin.chan@polymtl.ca>
2019-02-26 17:21:00 -0500

    calculation duree deplacement

--- src/app/formulaires/voyage-form/voyage-form.component.ts
+++ src/app/formulaires/voyage-form/voyage-form.component.ts
@@ -8,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 
 export class VoyageFormComponent implements OnInit {
 
+  private duFilled = false;
+  private auFilled = false;
+  private bothFilled = false;
+  private du: any = null;
+  private au: any = null;
+  private dureeDeplacement: number = null;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  private duChange(str: any): void {
+    this.du = str;
+    this.duFilled = true;
+    this.updateDuree();
+  }
+
+  private auChange(str: any): void {
+    this.au = str;
+    this.auFilled = true;
+    this.updateDuree();
+  }
+
+  private updateDuree(): void {
+    this.bothFilled = this.duFilled && this.auFilled;
+    if (this.bothFilled) {
+      this.dureeDeplacement = this.au - this.du;
+      this.dureeDeplacement = (((this.dureeDeplacement/60)/60)/24)/1000;
+    }
   }
 
   currency = ["CAN", "US", "EURO", "GBP", "CHF", "BRL"];
