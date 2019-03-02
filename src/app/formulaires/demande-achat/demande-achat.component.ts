38b08e111de8ac8ef4afd1597df4f62c370bfba5
joeljpoulin <joel.poulin@polymtl.ca>
2019-03-01 19:43:35 -0500

    Descr. des items et ventilation budg. dynamiques. Signatures.

--- src/app/formulaires/demande-achat/demande-achat.component.ts
+++ src/app/formulaires/demande-achat/demande-achat.component.ts
@@ -10,18 +10,18 @@ import {MatTableDataSource} from '@angular/material';
 
 export class DemandeAchatComponent implements OnInit {
   options: FormGroup;
-  montant :number = 0;
-  statutVersement : number = 0;
-  ventilation =
-    [
-    {id:0, ubr:"", compte:"", unite:"", percent:0, montant:0, },
-    {id:1, ubr:"", compte:"", unite:"", percent:0, montant:0, },
-    {id:2, ubr:"", compte:"", unite:"", percent:0, montant:0, },
-    ];
-  ventilationTotal :number = 0;
+  montant = 0;
+  statutVersement = 0;
+  ventilation = [{id: 0, ubr: '', compte: '', unite: '', percent: 0, montant: 0}];
+  items = [{id: 0, ref: 0, descr: '', quant: 0, prixUnit: 0, totalItems: 0}];
+  ventilationTotal = 0;
   dSventilation = new MatTableDataSource(this.ventilation);
-  displayedColumns: string[] = ['ubr', 'compte', 'unite', 'percent', 'montant', 'action'];
-  rowID: number = 2;
+  dSitems = new MatTableDataSource(this.items);
+  displayedColumnsVentilation: string[] = ['ubr', 'compte', 'unite', 'percent', 'montant', 'action'];
+  displayedColumnsItems: string[] = ['ref', 'descr', 'quant', 'prixUnit', 'totalItems', 'action'];
+  rowIDVentilation = 2;
+  rowIDItems = 2;
+  itemsTotal = 0;
 
   constructor(fb: FormBuilder) {
     this.options = fb.group({
@@ -30,20 +30,23 @@ export class DemandeAchatComponent implements OnInit {
     });
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  onChangeStatus(value){
+  onChangeStatus(value) {
     this.statutVersement = value;
   }
 
-
   onCreate() {
     this.ventilation.push(
-      {id:this.rowID, ubr:"", compte:"", unite:"", percent:0, montant:0, }
+      {id: this.rowIDVentilation, ubr: '', compte: '', unite: '', percent: 0, montant: 0, }
     );
-    this.rowID++;
+    this.items.push(
+      {id: this.rowIDItems, ref: 0, descr: '', quant: 0, prixUnit: 0, totalItems: 0 }
+    );
+    this.rowIDVentilation++;
+    this.rowIDItems++;
     this.dSventilation._updateChangeSubscription();
+    this.dSitems._updateChangeSubscription();
     this.updateTotal();
   }
 
@@ -54,6 +57,7 @@ export class DemandeAchatComponent implements OnInit {
       }
     }
     this.dSventilation._updateChangeSubscription();
+    this.dSitems._updateChangeSubscription();
     this.updateTotal();
   }
 
@@ -61,6 +65,9 @@ export class DemandeAchatComponent implements OnInit {
     this.ventilationTotal = 0;
     this.ventilation.forEach(element => {
       this.ventilationTotal += element.montant;
+    });
+    this.items.forEach(element => {
+      this.itemsTotal += element.prixUnit;
     });
   }
 
