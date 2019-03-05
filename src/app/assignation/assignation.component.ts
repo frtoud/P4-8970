5228d12eebe7ece9d00f6c137b34ebb9a6ef9f73
joeljpoulin <joel.poulin@polymtl.ca>
2019-03-05 10:03:00 -0500

    Synchronise mat-table et dialog.

--- src/app/assignation/assignation.component.ts
+++ src/app/assignation/assignation.component.ts
@@ -3,56 +3,48 @@ import {MatDialogRef, MatTableDataSource, MAT_DIALOG_DATA} from '@angular/materi
 import {MatDialog} from '@angular/material';
 
 export interface DialogData {
-  animal: string;
-  name: string;
+  participants: string[];
 }
 @Component({
   selector: 'app-assignation',
   templateUrl: './assignation.component.html',
   styleUrls: ['./assignation.component.css']
 })
+
 export class AssignationComponent implements OnInit {
-  participant = [{id: 0}];
+  participant = [{name: ''}];
   dSparticipant = new MatTableDataSource(this.participant);
-  displayedColumnsVentilation: string[] = ['participants'];
   rowIDParticipants = 2;
-  animal: string;
-  name: string;
+  displayedColumnParticipants: string[] = ['participants'];
+
   constructor(public dialog: MatDialog) {
   }
   ngOnInit() {
   }
-  onCreate() {
-    this.participant.push(
-      {id: this.rowIDParticipants}
-    );
-    this.rowIDParticipants++;
-    this.dSparticipant._updateChangeSubscription();
-  }
   openDialog(): void {
     const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
       width: '250px',
-      data: {name: this.name, animal: this.animal}
+      data: {participants: this.participant}
     });
-
     dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-      this.animal = result;
+      this.participant.push(
+        {name: result}
+      );
+      this.rowIDParticipants++;
+      this.dSparticipant._updateChangeSubscription();
     });
   }
 }
 
 @Component({
   selector: 'dialog-overview-example-dialog',
-  templateUrl: 'dialog-overview-example-dialog.html',
+  templateUrl: 'participants-dialog.html',
 })
 
 export class DialogOverviewExampleDialog {
-
   constructor(
     public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
     @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
-
   onNoClick(): void {
     this.dialogRef.close();
   }
