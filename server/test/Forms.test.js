bc8268eead00ef30580fea93686efdb5ee41b722
joeljpoulin <joel.poulin@polymtl.ca>
2019-03-24 18:08:42 -0400

    formsManager test

--- server/test/Forms.test.js
+++ server/test/Forms.test.js
@@ -1,24 +1,57 @@
 var expect = require('chai').expect;
 var mongoose = require('mongoose');
-var User = require('../models/Users');
-var userSchema = new mongoose.Schema({
-    firstName: { type: String, required: true },
-lastName: { type: String, required: true },
-email: { type: String, required: true },
-hash: { type: String, required: true },
-type: { type: String, required: true },
-linkExpiration: { type: Date, required: true },
-createdAt: {type: Date, required: true},
-updatedAt: {type: Date, required: true{}
-);
+var Forms = require('../models/Forms');
+var factories = require('./factories');
+var formsManager = require('../managers/formsManager');
 
-describe('users', function() {
-    it('should be invalid if name is empty', function(done) {
-        var u = new User();
-        m.validate(function(err) {
-            expect(err.errors.name).to.exist;
-            done();
-        });
+const collaborateurSchema = new Schema({
+    firstName: { type: String, required: true },
+    nom: { type: String, required: true },
+    email: { type: String, required: true },
+    ordre: { type: Number, required: true },
+    access: { type: String, required: true }
+});
+
+var formsSchema = new mongoose.Schema({
+    firstName: { type: String, required: true },
+    idForm: { type: String, required: true },
+    auteur: { type: Object, required: true },
+    nomFormulaire: { type: String, required: true },
+    collaborateurs: { type: [collaborateurSchema], required: true },
+    statut: { type: String, required: true },
+    data: { type: Schema.Types.Mixed, required: true },
+    creeLe: { type: Date, required: true },
+    modifieLe: { type: Date, required: true }
+});
+
+describe('forms', function() {
+    beforeEach(function() {
+        sinon.stub(formsManager, 'getFormById');
+    });
+
+    afterEach(function() {
+        Forms.getFormById.restore();
+    });
+    it('getFormById should be called with valid params', function(done) {
+        var f = factories.validForm();
+        Forms.getFormById.yields(null, f);
+        var req = { params: {}};
+        var res = {
+            send: sinon.stub()
+        };
+        sinon.assert.calledWith(res.send, f)
     });
 });
+
+
+// describe('forms.methos.setCreatedAt', function() {
+//     it('should be invalid if name is empty', function(done) {
+//         var f = new Forms();
+//         f.setCreatedAt()(function(err) {
+//                 expect(typeof(f.creeLe) === "Date");
+//             });
+//     });
+// });
+
+
 
---
+++ server/test/factories.js
@@ -0,0 +1,23 @@
+// module.exports.validUser = function() {
+//     return {
+//         idForm: '1',
+//         auteur: 'auteur',
+//         nomFormulaire:'nomFormulaire',
+//         collaborateurs: [],
+//         statut: '',
+//         creeLe: new Date(),
+//         modifieLe: new Date()
+//     };
+// };
+
+module.exports.validForm = function() {
+    return {
+        idForm: '1',
+        auteur: 'auteur',
+        nomFormulaire:'nomFormulaire',
+        collaborateurs: [],
+        statut: '',
+        creeLe: new Date(),
+        modifieLe: new Date()
+    };
+};
