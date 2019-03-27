d2151673265fd4b3d73d162d9aa3d52157bcbe91
joeljpoulin <joel.poulin@polymtl.ca>
2019-03-27 13:07:50 -0400

    Adds missing jest files.

--- server/__tests__/api/api_spec.js
+++ server/__tests__/api/api_spec.js
@@ -1,6 +1,7 @@
 const frisby = require('frisby');
+const BASE_PATH_FORMS = 'http://formulaires.info.polymtl.ca/forms';
 
 it('should be a teapot', function () {
-    return frisby.get('http://httpbin.org/status/418')
+    return frisby.get(BASE_PATH_FORMS + )
         .expect('status', 418);
 });
