790a0dbbe30a05c6a10e64235dfd2813d40e4cbc
Francois Toulouse <francois.toulouse@polymtl.ca>
2019-03-29 16:06:29 -0400

    Rest in Recyclebin, Mock users

--- client/src/app/services/users.service.ts
+++ client/src/app/services/users.service.ts
@@ -22,20 +22,9 @@ export class UserService {
         return Promise.reject(error.feedbackMessage || error);
     }
 
-    getAllUsers() :Promise<User[]>
-    {
+    getAllUsers(): Promise<User[]> {
         return this.http.get<User>(`${Config.apiUrl}/users`).toPromise()
         .then(user => user)
         .catch(UserService.handleError);
-    }
-    mockUsers() :Promise<User[]>
-    {
-        return Promise.resolve([
-            { _id:"1", firstName: "A", lastName: "AA", email:"a@a.a", type:"ADMIN"},
-            { _id:"2", firstName: "B", lastName: "AD", email:"b@a.a", type:"ADMIN"},
-            { _id:"3", firstName: "C", lastName: "BA", email:"c@a.a", type:"ADMIN"},
-            { _id:"4", firstName: "D", lastName: "AC", email:"d@a.a", type:"ADMIN"},
-            { _id:"5", firstName: "E", lastName: "EE", email:"e@a.a", type:"ADMIN"},
-        ]);
     }
 }
