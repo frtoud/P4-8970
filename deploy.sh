81cf07a88188d1e9df751a107da3b2745fe314ad
joeljpoulin <joel.poulin@polymtl.ca>
2019-03-29 11:13:37 -0400

    Test admin first user.

--- deploy.sh
+++ deploy.sh
@@ -21,4 +21,14 @@ export class Config {
     public static apiUrl = 'http://localhost:443';
   }
 EOF
+sudo mongo use users
+sudo mongo --eval 'var admin =  {
+        "_id": "5c8177712e64ab2736875bc7",
+        "firstName": "admin".
+        "lastName": "",
+        "email": "",
+        "type": "ADMIN",
+        "createdAt": "",
+        "updatedAt": ""
+    }; db.users.insert(user);'
 sudo ng serve --host 0.0.0.0 --port 80 --disable-host-check &
