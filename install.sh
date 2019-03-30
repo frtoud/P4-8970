b37f9cb2d34b03f543f11a1090d8bc057270899b
joeljpoulin <joel.poulin@polymtl.ca>
2019-03-30 15:26:41 -0400

    Redhat server 7 for mongo script.

--- install.sh
+++ install.sh
@@ -39,7 +39,7 @@ sudo touch mongodb-org-4.0.repo
 sudo cat <<EOF > ./mongodb-org-4.0.repo
 [mongodb-org-4.0]
 name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.0/x86_64/
+baseurl=https://repo.mongodb.org/yum/redhat/7/mongodb-org/4.0/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.0.asc
