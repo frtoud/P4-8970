850cf3dfd0596164906db3db86bfb3dbfb3777be
Vincent Larocque <vinlarocque@gmail.com>
2019-04-03 13:23:00 -0400

    toggle pour utiliser la db de francois

--- server/lib/db.js
+++ server/lib/db.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
 //Configure Mongoose
-mongoose.connect('mongodb://test:test123@ds147344.mlab.com:47344/projet4-test');
+//mongoose.connect('mongodb://test:test123@ds147344.mlab.com:47344/projet4-test');
 //Francois' MongoDB Atlas cluster
-//mongoose.connect('mongodb+srv://test-user:25565@projet4-cluster-tgaf8.mongodb.net/test?retryWrites=true');
+mongoose.connect('mongodb+srv://test-user:25565@projet4-cluster-tgaf8.mongodb.net/test?retryWrites=true');
 mongoose.set('debug', true);
